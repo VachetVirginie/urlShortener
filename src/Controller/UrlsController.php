eeb0b3c4ffbcacf84c18dfc1177faba3d31886d9
Vivi <vachetvirginie@gmail.com>
2022-01-05 20:59:56 +0100

    Handle form and add validation constraint

--- src/Controller/UrlsController.php
+++ src/Controller/UrlsController.php
@@ -4,22 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Url;
+
 
 class UrlsController extends AbstractController
 {
     #[Route('/', name: 'app_urls_create')]
-    public function create(): Response
+    public function create(Request $request): Response
     {
        $form = $this->createFormBuilder()
             ->add('original', null, [
                 'label' => false,
                 'attr' => [
                     'placeholder' => 'Enter the url to shortener here'
+                ],
+                'constraints' => [
+                    new NotBlank(['message' =>'You must enter an Url.']),
+                    new Url(['message' => 'Url entered is invalid.'])
                 ]
             ])
             ->getForm()
        ;
+
+       $form->handleRequest($request);
+
+       if ($form->isSubmitted() && $form->isValid()){
+
+       }
 
         return $this->render('urls/create.html.twig', [
             'controller_name' => 'UrlsController',
