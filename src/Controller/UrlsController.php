058b6267ecae8e32d4fb13643d5b40e3ddcc6068
Vivi <vachetvirginie@gmail.com>
2022-01-05 20:33:38 +0100

    add form

--- src/Controller/UrlsController.php
+++ src/Controller/UrlsController.php
@@ -11,8 +11,19 @@ class UrlsController extends AbstractController
     #[Route('/', name: 'app_urls_create')]
     public function create(): Response
     {
+       $form = $this->createFormBuilder()
+            ->add('original', null, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Enter the url to shortener here'
+                ]
+            ])
+            ->getForm()
+       ;
+
         return $this->render('urls/create.html.twig', [
             'controller_name' => 'UrlsController',
+            'form' => $form->createView()
         ]);
     }
 }
